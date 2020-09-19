4e143b7dbea358f1441b14fd664cb0e42cfa1ce5
Konsta Kutvonen <kutvok@amazon.com>
2020-09-19 11:07:27 +0300

    Initial Commit

---
+++ pastaa/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  pastaa
+//
+//  Created by Kutvonen, Konsta on 9/19/20.
+//  Copyright © 2020 Kutvonen, Konsta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
